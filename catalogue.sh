c5d82a26d425ea0b7f03820bfc98c5fb14b39d47
vkatt@VIGNESH-PC <vkattamudi88@gmail.com>
2025-11-27 11:20:47 +0000

    roboshop

--- catalogue.sh
+++ catalogue.sh
@@ -6,7 +6,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1) # or $(basename "$0" .sh)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 SCRIPT_DIR=$PWD
 
