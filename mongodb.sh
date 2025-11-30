b18a620d5d082c19e3a3441cc42e4a9b8de83f48
vkatt@VIGNESH-PC <vkattamudi88@gmail.com>
2025-11-30 17:38:03 +0000

    Roboshop

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+START_TIME=$(date +%s)
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
@@ -51,6 +52,11 @@ systemctl restart mongod &>>$LOG_FILE
 VALIDATE $? "restarting mongodb"
 
 echo "MongoDB Setup Completed Successfully."
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
 
 # #!/bin/bash
 
