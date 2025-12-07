87779cc5ea5e12994842afc14fee67fbb2a6ea9b
vkatt@VIGNESH-PC <vkattamudi88@gmail.com>
2025-12-07 15:18:09 +0000

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -6,8 +6,8 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 ZONE_ID="Z0443285370E15R10QIN" # replace with your ZONE ID
 DOMAIN_NAME="dsops84.space" # replace with your domain
 
-#for instance in $@ 
-for instance in ${INSTANCES[@]}
+for instance in $@ 
+#for instance in ${INSTANCES[@]}
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
     PRIVATE_IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
